0676f339e53ddd54ade53eb5dad9ed477fc5854e
mike connor <mike@mikeconnor.tech>
2023-04-30 19:08:14 -0500

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import react from '@astrojs/react';
 import svelte from '@astrojs/svelte';
 import vue from '@astrojs/vue';
 import solid from '@astrojs/solid-js';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
 	// Enable many frameworks to support all different kinds of components.
 	integrations: [preact(), react(), svelte(), vue(), solid()],
+	output: 'server',
+	adapter: vercel(),
 });
